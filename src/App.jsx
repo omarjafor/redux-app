449972eeec22bcff32e76075f02124b7929e16fd
omarjafor <omarjaforchy@gmail.com>
2024-01-21 11:27:23 +0600

    redux error getting not solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
+import { useDispatch, useSelector } from "react-redux";
+import { incNumber, decNumber } from './actions/index';
 
 function App() {
+
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
 
   return (
     <>
       <h1 className="text-center text-4xl  my-4 font-bold">Redux App Increment & Decrement</h1>
       <div className="text-black font-bold text-2xl space-x-4 p-2 text-center">
-        <a href="" title="Decrement" className="hover:bg-slate-500 bg-lime-500 px-4 py-2 rounded-xl hover:text-white"><span>-</span></a>
-        <input type="text" name="quantity" value='0' className="w-12 text-center p-2 rounded-xl bg-slate-400" />
-        <a href="" title="Increment" className="hover:bg-slate-500 bg-lime-500 px-4 py-2 rounded-xl hover:text-white"><span>+</span></a>
+        <button title="Decrement" className="hover:bg-slate-500 bg-lime-500 px-4 py-2 rounded-xl hover:text-white" onClick={() => dispatch(decNumber())}><span>-</span></button>
+        <input type="text" name="quantity" value={myState} className="w-12 text-center p-2 rounded-xl bg-slate-400" />
+        <button title="Increment" className="hover:bg-slate-500 bg-lime-500 px-4 py-2 rounded-xl hover:text-white" onClick={() => dispatch(incNumber())}><span>+</span></button>
       </div>
     </>
   )
