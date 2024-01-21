38d52e27ab2340554be29be5df6113b047a44b87
omarjafor <omarjaforchy@gmail.com>
2024-01-21 19:30:08 +0600

    incNumber decNumber completed

--- src/store.js
+++ src/store.js
@@ -4,4 +4,3 @@ import rootReducer from './reducers/index';
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-export default store;
